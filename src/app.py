f921210169d10ad978452807a4a6005a70a14046
Nicola Gutierrez <nicolagutierrez.ng@gmail.com>
2024-03-30 11:35:40 +0100

    fix: minor adjustments

--- src/app.py
+++ src/app.py
@@ -11,27 +11,44 @@ icons = {
     "crontab": "crontab-ui.png",
 }
 
-homepage_path = os.getenv("HOMEPAGE_PATH, homepage/config/services.yaml")
+homepage_path = os.getenv("HOMEPAGE_PATH", "homepage/config/services.yaml")
 pattern = r"Host\([^)]*\)"
 app = Flask(__name__)
 
+
 def is_custom_service(rule):
     return re.search(pattern, rule) is not None
+
 
 ##* APIs *##
 @app.route("/api/v1/homepage/update", methods=["PUT"])
 def homepage_update():
     # Open the YAML file for reading
-    with open(homepage_path, "r") as yaml_file:
+    with open(str(homepage_path), "r") as yaml_file:
         data = yaml.safe_load(yaml_file)
-    
-    response = requests.get('http://web1.elemento.cloud:8080/api/http/routers') #ToDo: generalize the URL of traefik
+
+    response = requests.get(
+        "http://web1.elemento.cloud:8080/api/http/routers"
+    )  # ToDo: generalize the URL of traefik
+
     routers = response.json()
+    keys = [list(service.keys())[0] for service in data[0]["Services"]]
     for router in routers:
         name = router["name"].split("@")[0]
-        if is_custom_service(router["rule"]) and name not in data[0]["Services"] and name != "homepage":
-          icon = icons[name] if name in icons else ""
-          data[0]["Services"].append({name: {"icon": icon,"href": router["rule"].split('`')[1]}})
+        if (
+            is_custom_service(router["rule"])
+            and name.capitalize() not in keys
+            and name != "homepage"
+        ):
+            icon = icons[name] if name in icons else ""
+            data[0]["Services"].append(
+                {
+                    name.capitalize(): {
+                        "icon": icon,
+                        "href": "http://" + router["rule"].split("`")[1],
+                    }
+                }
+            )
 
     with open("test.yaml", "w") as yaml_file:
         yaml.dump(data, yaml_file)
