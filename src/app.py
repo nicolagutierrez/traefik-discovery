51ee8040088e59910118ba7d62944a444c6ad4db
Nicola Gutierrez <nicolagutierrez.ng@gmail.com>
2024-03-30 12:12:01 +0100

    chore: enhance of APIs

--- src/app.py
+++ src/app.py
@@ -21,40 +21,52 @@ def is_custom_service(rule):
 
 
 ##* APIs *##
-@app.route("/api/v1/homepage/update", methods=["PUT"])
+@app.route("/api/v1/status", methods=["GET"])
+def check_server():
+    return jsonify({"message": "Traefik discovery is running"})
+
+@app.route("/api/v1/homepage/update", methods=["POST"])
 def homepage_update():
-    # Open the YAML file for reading
-    with open(str(homepage_path), "r") as yaml_file:
-        data = yaml.safe_load(yaml_file)
+    try: 
 
-    response = requests.get(
-        "http://web1.elemento.cloud:8080/api/http/routers"
-    )  # ToDo: generalize the URL of traefik
+        # Open the YAML file for reading
+        with open(str(homepage_path), "r") as yaml_file:
+            data = yaml.safe_load(yaml_file)
 
-    routers = response.json()
-    if data[0]["Services"] == None:
-        data[0]["Services"] = []
+        response = requests.get(
+            "http://web1.elemento.cloud:8080/api/http/routers"
+        )  # ToDo: generalize the URL of traefik
 
-    keys = [list(service.keys())[0] for service in data[0]["Services"]]
-    for router in routers:
-        name = router["name"].split("@")[0]
-        if (
-            is_custom_service(router["rule"])
-            and name.capitalize() not in keys
-            and name != "homepage"
-        ):
-            icon = icons[name] if name in icons else ""
-            data[0]["Services"].append(
-                {
-                    name.capitalize(): {
-                        "icon": icon,
-                        "href": "http://" + router["rule"].split("`")[1],
+        routers = response.json()
+        if data[0]["Services"] == None:
+            data[0]["Services"] = []
+
+        keys = [list(service.keys())[0] for service in data[0]["Services"]]
+        for router in routers:
+            name = router["name"].split("@")[0]
+            if (
+                is_custom_service(router["rule"])
+                and name.capitalize() not in keys
+                and name != "homepage"
+            ):
+                icon = icons[name] if name in icons else ""
+                data[0]["Services"].append(
+                    {
+                        name.capitalize(): {
+                            "icon": icon,
+                            "href": "http://" + router["rule"].split("`")[1],
+                        }
                     }
-                }
-            )
+                )
 
-    with open(str(homepage_path), "w") as yaml_file:
-        yaml.dump(data, yaml_file)
+        with open(str(homepage_path), "w") as yaml_file:
+            yaml.dump(data, yaml_file)
+        
+        return jsonify(data[0]["Services"])
+
+    except requests.exceptions.RequestException as e:
+        app.logger.info(f"Failed to update services, status code: 500 \n error: {e}")
+        return jsonify({"error": f"Update services failed: {e}"}), 500
 
 
 ##* APP (just for testing purpose - use flask run instead) *##
